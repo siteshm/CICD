0ac49a9cadd4b3138a650fc9a2de1ead1e23613f
siteshm <38156774+siteshm@users.noreply.github.com>
2021-06-16 18:26:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,13 @@
 const express = require('express');
 
 // Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 3000;
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
