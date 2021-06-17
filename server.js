a9935c007861d82759c01740d919bf39190b963c
siteshm <38156774+siteshm@users.noreply.github.com>
2021-06-17 11:00:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const app = express();
 app.get('/', function (req, res) {
   res.send('Hello world\n');
 });
+app.get('/health-check', function (req, res) {
+  res.send('Health Check Passed\n');
+});
+app.get('/bad-health', function (req, res) {
+  res.status(500).send('Health Check did not Passed\n');
+});
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
