649cab5ad9aa8671549e6f0bf3c910ab872d90f4
siteshm <38156774+siteshm@users.noreply.github.com>
2021-06-18 10:42:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world v3\n');
+  res.send('Hello world v4\n');
 });
 app.get('/health-check', function (req, res) {
   res.send('Health Check Passed\n');
